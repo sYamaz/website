a915550a362f2458a2a8eae811f404ca32242ea6
Shun Yamazaki <syunn820@gmail.com>
2021-12-29 07:23:38 +0900

    rename workspace

---
+++ website/Pages/Works/Datas/AnyAppData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace website.Pages.Works.Datas
+{
+
+    public record AnyAppData
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string ImagePath { get; set; } = "";
+        public string ReadMoreURL { get; set; } = "";
+
+        public SupportPlatform SupportPlatform { get; set; } = SupportPlatform.none;
+
+        public AppStatus Status { get; set; } = AppStatus.underDevelop;
+    }
+
+    [Flags]
+    public enum SupportPlatform
+    {
+        none = 0b00000000,
+        iOS = 0b00000001,
+        macOS = 0b00000010,
+        windows = 0b00000100,
+        linux = 0b00001000,
+        android = 0b00010000
+    }
+
+    public enum AppStatus
+    {
+        underDevelop,
+        inReview,
+        release,
+        endOfLife
+    }
+}
+
